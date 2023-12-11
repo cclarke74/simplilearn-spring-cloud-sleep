b391961b511fa905af8440466c3c556c686fa7c1
cclarke74 <clairc2@gmail.com>
2023-12-11 14:22:26 -0800

    first commit

---
+++ src/main/java/com/simplilearn/spring/SimplilearnSpringCloudSleepApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplilearnSpringCloudSleepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplilearnSpringCloudSleepApplication.class, args);
+	}
+
+}
