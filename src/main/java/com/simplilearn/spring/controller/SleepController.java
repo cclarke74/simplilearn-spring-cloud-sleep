baa87b68e836021feef60b30bbe23792f7080f83
cclarke74 <clairc2@gmail.com>
2023-12-14 19:59:42 -0800

    Added cofig

--- src/main/java/com/simplilearn/spring/controller/SleepController.java
+++ src/main/java/com/simplilearn/spring/controller/SleepController.java
@@ -9,30 +9,42 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.simplilearn.spring.bean.Sleep;
+import com.simplilearn.spring.config.Config;
 import com.simplilearn.spring.service.SleepService;
 
 @RestController
 public class SleepController {
 
-	final Logger logger = LoggerFactory.getLogger(this.getClass());
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	static final String PORT_PROPERTY = "local.server.port";
-	
-	@Autowired
-	Environment environment;
-	
-	@Autowired
-	SleepService sleepService;
-	
-	@GetMapping("/sleep/{virth}") // format: yyyy-MM-dd
-	Sleep getSleepRecommendation(@PathVariable String birth) {
-		
-		logger.debug("Getting Sleep Recommendation, birth: {}", birth);
-		
-		String port = this.environment.getProperty(PORT_PROPERTY);
-		String recommendation = this.sleepService.getSleepRecommendation(birth);
-		
-		return new Sleep(birth, recommendation, port);
-	}
-	
+    static final String PORT_PROPERTY = "local.server.port";
+    
+    @Autowired
+    Config config;
+    
+    @Autowired
+    Environment environment;
+
+    @Autowired
+    SleepService sleepService;
+
+    @GetMapping("/sleep/{birth}") // format: yyyy-MM-dd
+    Sleep getSleepRecommendation(@PathVariable String birth) {
+
+    	logger.debug("Getting Sleep Recommendation, birth: {}", birth);
+    	
+    	String port = this.environment.getProperty(PORT_PROPERTY);
+    	String recommendation = this.sleepService.getSleepRecommendation(birth);
+    	
+    	return new Sleep(birth,recommendation,port);
+    	
+    }
+
+    @GetMapping("/sleep/config") 
+    Config getSleepConfig() {
+    	
+    	logger.debug("Getting Sleep Configuration");
+    	    	
+    	return config;
+    }
 }
--- src/main/java/com/simplilearn/spring/service/SleepService.java
+++ src/main/java/com/simplilearn/spring/service/SleepService.java
@@ -3,60 +3,54 @@ package com.simplilearn.spring.service;
 import java.time.LocalDate;
 import java.time.Period;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.simplilearn.spring.config.Config;
 
 @Service
 public class SleepService {
-		
-	public String getSleepRecommendation(String birth) {
-		
-		LocalDate now = LocalDate.now();
-		LocalDate dob = LocalDate.parse(birth);
-		
-		if(dob.isAfter(now)) {
-			throw new RuntimeException("Invalid date of birth!, birth:" + birth);
-		}
-		
-		Period period = Period.between(dob, now);
-		
-		if(period.getYears() >= 65) { // Adult
-			return "7-9 hours";
-		}
-		else if (period.getYears() >= 61) { // Adult
-			return "7-8 hours";
-			
-		}
-		else if (period.getYears() >= 61) { // Adult
-			return "7-8 hours";
-			
-		}
-		else if (period.getYears() >= 18) { // Adult
-			return "7 or more hours per night";
-		}
-			else if (period.getYears() >= 13) { // Teen: 13-18 years
-				return "8-10 hours per 24 hours";
-			
-		}
-			else if (period.getYears() >= 6) { // School Age: 6-12 years
-				return "9-12 hours per 24 hours";
-				
-			}
-			else if (period.getYears() >= 3) { // PreSchool Age: 3-5 years
-				return "10-13 hours per 24 hours (including naps)";
-				
-			}
-			else if (period.getYears() >= 1) { // Toddler: 1-2 years
-				return "11-14 hours per 24 hours (including naps)";
-				
-			}
-			else if (period.getMonths() >= 4) { // Infant: 4-12 months
-				return "12-16 hours per 24 hours (including naps)";
-				
-			}
-			else  { // Newborn: 0-3 months
-				return "14-17 hours (National Sleep Foundation)";
-				
-			}
-		}
 
+    @Autowired
+    Config config;
+
+    public String getSleepRecommendation(String birth) {
+
+        LocalDate now = LocalDate.now();
+        LocalDate dob = LocalDate.parse(birth);
+
+        if (dob.isAfter(now)) {
+            throw new RuntimeException("Invalid date of birth!, birth: " + birth);
+        }
+
+        Period period = Period.between(dob, now);
+
+        if (period.getYears() >= config.getAdults3Age()) { /* Adult */
+            return config.getAdults3Msg();
+        }
+        else if (period.getYears()>= config.getAdults2Age()) { /* Adult */
+            return config.getAdults2Msg();
+        }
+        else if (period.getYears()>= config.getAdults1Age()) { /* Adult */
+            return config.getAdults1Msg();
+        }
+        else if (period.getYears()>= config.getTeenAge()) { /* Teen: 13-18 years */
+            return config.getTeenMsg();
+        }
+        else if (period.getYears()>= config.getSchoolAge()) { /* School Age: 6-12 years */
+            return config.getSchoolMsg();
+        }
+        else if (period.getYears()>= config.getPreschoolAge()) { /* PreSchool Age: 3-5 years */
+            return config.getPreschoolMsg();
+        }
+        else if (period.getYears()>= config.getToddlerAge()) { /* Toddler: 1-2 years */
+            return config.getToddlerMsg();
+        }
+        else if (period.getMonths()>= config.getInfantAge()) { /* Infant: 4-12 months */
+            return config.getInfantMsg();
+        }
+        else { /* Newborn: 0-3 months */
+            return config.getNewbornMsg();
+        }
+    }
 }
